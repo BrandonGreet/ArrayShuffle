7d32a9dfa97d3660797b2e01066769667c3cc27e
BrandonGreet <greet.brandon@yahoo.com>
2018-03-24 21:00:16 -0400

    Completed program with hard-coded values.

---
+++ ArrayShuffle/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ArrayShuffle
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
